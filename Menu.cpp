6e6b9805029df0076a308087917be1e2528624ae
Maxwell Sanders <maxwell.sanders@mavs.uta.edu>
2016-10-10 22:27:56 -0500

    -further updated the menu, added actual options

--- Menu.cpp
+++ Menu.cpp
@@ -2,7 +2,10 @@
 //this class is in charge of the menu
 #include "Menu.h"
 
+#define INVALID response<0||response>2
+
 using namespace std;
+
 
 //used to see who the user is
 int Menu::getPerm(){
@@ -16,12 +19,55 @@ void Menu::setPerm(int perm){
 
 //if this returns true it will exit overall, otherwise it will return to login
 bool Menu::mainMenu(int perm){
+    int response = 1337;
     setPerm(perm);
-    if(getPerm() == 1){
-	cout << "Welcome Rob Robman!\n";
-    }
     if(getPerm() == 0){
-	cout << "Welcome Grunt.\n";
+	cout << "\nWelcome Grunt.\n";
     }
-    return true;
+    if(getPerm() == 1){
+	cout << "\nWelcome Rob Robman!\n";
+    }
+    while(INVALID){
+	cout << "What would you like to do?\n";
+	cout << "==========================\n";
+	cout << "==========================\n";
+	cout << "0 - exit\n";
+	cout << "1 - logout\n";
+	cout << "2 - use the workshop\n";
+	cin >> response;
+        if(INVALID || cin.fail()){
+	    cout << "That is an invalid task.\n\n";
+	    cin.clear();
+	    cin.ignore();
+	    response = 30;	
+        }
+
+	//exit
+	if(response == 0){
+	    return true;
+	}
+
+	//logout
+	if(response == 1){
+	    return false;
+	}
+
+	//use the workshop
+	if(response == 2){
+	    if(getPerm() == 1){
+	    	cout << "Hey you would be using the workshop if it was implemented.\n";
+	    	response = 1337;
+	    }
+	    else{
+		cout << "You don't have the permissions to do that.\n";
+	    	response = 1337;
+	    }
+	}
+	
+	//thats not a task
+	else{
+	    cout << "That's not a task!!! Please try again.\n";
+	}
+	
+    }
 }
